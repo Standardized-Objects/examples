07643282c779a73df07be5760445c328263a7e00
Juan Ezquerro <juan@syncrtc.com>
2020-03-13 00:22:10 +0100

    more stuff

--- stacks/base/install-macosx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Check for homebrew"
-type brew 2>&1 > /dev/null || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Check for iterm2"
-[ $TERM_PROGRAM = "iTerm.app" ] || brew cask install iterm2
-
